a123070ab99675d0d4540f88a01535a599e39721
MemoRuan <memoruan_27@hotmail.com>
2017-08-31 13:09:07 -0500

    Saving the changes for Story#8

--- helloWorld.java
+++ helloWorld.java
@@ -4,6 +4,8 @@ public class helloWorld {
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
         System.out.println("Hello World, this is me");
-    }
 
+        System.out.println("Ok, as the story#8 says, this is the implementation, so the question is \n"
+        + "--> Whis is the correct question to ask??? ");
+    }
 }
